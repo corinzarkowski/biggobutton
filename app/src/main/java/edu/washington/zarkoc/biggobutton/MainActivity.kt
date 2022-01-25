a53204a731c01781b53544cb2da5d9a7c282e531
corin <coreyz206@gmail.com>
2022-01-24 17:55:56 -0800

    finished

--- app/src/main/java/edu/washington/zarkoc/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/washington/zarkoc/biggobutton/MainActivity.kt
@@ -2,10 +2,31 @@ package edu.washington.zarkoc.biggobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        var width = displayMetrics.widthPixels
+        var height = displayMetrics.heightPixels
+
+        val pushButton = findViewById<Button>(R.id.button)
+        pushButton.layoutParams = LinearLayout.LayoutParams(width, height)
+        var clickCount = 0
+        pushButton.setOnClickListener {
+            clickCount += 1
+            if (clickCount == 1) {
+                pushButton.text = "You have pushed me 1 time!"
+            } else {
+                pushButton.text = "You have pushed me ${clickCount} times!"
+            }
+        }
     }
 }
